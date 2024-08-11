21448dfd4ebd9c115a2e2a4ac6cdf926408cbf30
Bijeshwor Sharma <bijeshwor2058@gmail.com>
2024-08-11 12:46:15 +0530

    Animation part is pending

--- scripts.js
+++ scripts.js
@@ -1,54 +1,99 @@
-const timerEl = document.getElementById("timer");
-const startButtonEl = document.getElementById("start");
-const stopButtonEl = document.getElementById("stop");
-const resetButtonEl = document.getElementById("reset");
+const playButton = document.getElementsByClassName("play")[0];
+const lapButton = document.getElementsByClassName("lap")[0];
+const resetButton = document.getElementsByClassName("reset")[0];
+const clearButton = document.getElementsByClassName("lap-clear-button")[0];
+const minute = document.getElementsByClassName("minute")[0];
+const second = document.getElementsByClassName("sec")[0];
+const centiSecond = document.getElementsByClassName("msec")[0];
+const laps = document.getElementsByClassName("laps")[0];
+const bg = document.getElementsByClassName("outer-circle")[0];
 
-let startTime = 0;
-let elapsedTime = 0;
-let timerInterval;
+console.log(bg);
+let isPlay = false;
+let secCounter = 0;
+let min;
+let sec;
+let centisec;
+let centiCounter = 0;
+let minCounter = 0;
+let lapItem = 0;
+let isReset = false;
 
-function startTimer() {
-  startTime = Date.now() - elapsedTime;
+const toggelButton = () => {
+  lapButton.classList.remove("hidden");
+  resetButton.classList.remove("hidden");
+};
 
-  timerInterval = setInterval(() => {
-    elapsedTime = Date.now() - startTime;
-    timerEl.textContent = formatTime(elapsedTime);
-  }, 10);
+const play = () => {
+  if (!isPlay && !isReset) {
+    playButton.innerHTML = "Pause";
+    bg.classList.add("animation-bg");
+    min = setInterval(() => {
+      minute.innerHTML = `${++minCounter}:`;
+    }, 60 * 1000);
 
-  startButtonEl.disabled = true;
-  stopButtonEl.disabled = false;
-}
+    sec = setInterval(() => {
+      if (secCounter === 60) {
+        secCounter = 0;
+      }
+      second.innerHTML = `&nbsp;${++secCounter}:`;
+    }, 1000);
 
-function formatTime(elapsedTime) {
-  const milliseconds = Math.floor((elapsedTime % 1000) / 10);
-  const seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
-  const minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
-  const hours = Math.floor(elapsedTime / (1000 * 60 * 60));
-  return (
-    (hours ? (hours > 9 ? hours : "0" + hours) : "00") +
-    ":" +
-    (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") +
-    ":" +
-    (seconds ? (seconds > 9 ? seconds : "0" + seconds) : "00") +
-    "." +
-    (milliseconds > 9 ? milliseconds : "0" + milliseconds)
-  );
-}
-function stopTimer() {
-  clearInterval(timerInterval);
-  startButtonEl.disabled = false;
-  stopButtonEl.disabled = true;
-}
-function resetTimer() {
-  clearInterval(timerInterval);
+    centisec = setInterval(() => {
+      if (centiCounter === 100) {
+        centiCounter = 0;
+      }
+      centiSecond.innerHTML = `&nbsp;${++centiCounter}`;
+    }, 10);
 
-  elapsedTime = 0;
-  timerEl.textContent = "00:00:00";
+    isPlay = true;
+    isReset = true;
+  } else {
+    playButton.innerHTML = "Play";
+    clearInterval(min);
+    clearInterval(sec);
+    clearInterval(centisec);
+    isPlay = false;
+    isReset = false;
+    bg.classList.remove("animation-bg");
+  }
+  toggelButton();
+};
 
-  startButtonEl.disabled = false;
-  stopButtonEl.disabled = true;
-}
+const reset = () => {
+  lapButton.classList.add("hidden");
+  resetButton.classList.add("hidden");
+  second.innerHTML = "&nbsp;0 :";
+  centiSecond.innerHTML = "&nbsp;0";
+  minute.innerHTML = "0 :";
+};
 
-startButtonEl.addEventListener("click", startTimer);
-stopButtonEl.addEventListener("click", stopTimer);
-resetButtonEl.addEventListener("click", resetTimer);
+const lap = () => {
+  const li = document.createElement("li");
+  const number = document.createElement("span");
+  const timeStamp = document.createElement("span");
+
+  li.setAttribute("class", "lap-item");
+  number.setAttribute("class", "number");
+  timeStamp.setAttribute("class", "time-stamp");
+
+  number.innerText = `#${++lapItem}`;
+  timeStamp.innerHTML = `${minCounter}: ${secCounter}: ${centiCounter}`;
+
+  li.append(number, timeStamp);
+  laps.append(li);
+
+  clearButton.classList.remove("hidden");
+};
+
+const clearAll = () => {
+  laps.innerHTML = "";
+  laps.append(clearButton);
+  clearButton.classList.add("hidden");
+  lapItem = 0;
+};
+
+playButton.addEventListener("click", play);
+resetButton.addEventListener("click", reset);
+lapButton.addEventListener("click", lap);
+clearButton.addEventListener("click", clearAll);
